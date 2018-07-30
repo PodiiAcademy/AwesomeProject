9805d3544bfcca95a8547df119f2c4613c28dc72
GettyOrawo <gettyorawo@gmail.com>
2018-07-30 13:23:18 +0300

    Learn State

--- App.js
+++ App.js
@@ -1,21 +1,33 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
+import React, { Component } from 'react';
+import { AppRegistry, Text, View } from 'react-native';
+class Blink extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+    setInterval(() => {
+      this.setState(previousState =>{
+      return {isShowingText: !previousState.isShowingText};
+    });
+    }, 1000);
+  }
+  render(){
+    let display = this.state.isShowingText ? this.props.text:' ';
+    return(
+    <Text>{display}</Text>
+    );
+  }
+}
+export default class BlinkApp extends Component {
+  render(){
+    return(
+    <View>
+      <Blink text='Nimetembea Kwote Kwote'/>
+      <Blink text='Nimezunguka Kwote Kwote'/>
+      <Blink text='Nimetafuta Kwote Kwote'/>
+      <Blink text='Hakuna na hata Kuwepo!!'/>
       </View>
     );
   }
 }
+AppRegistry.registerComponent('AwesomeProject', () => BlinkApp);
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});

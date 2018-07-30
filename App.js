8027cf4fbffae13892be22a3bfb7b2048db3fce7
GettyOrawo <gettyorawo@gmail.com>
2018-07-30 13:36:05 +0300

    Learn Style

--- App.js
+++ App.js
@@ -1,21 +1,30 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class LotsOfStyles extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
+      <View>
+        <Text style={styles.red}>Hello World Im red!</Text>
+        <Text style={styles.blue}>Hello World Im blue!</Text>
+        <Text style={[styles.red, styles.blue]}>Hello World I am.. BLUE AGAIN!</Text>
+        <Text style={[styles.blue, styles.yellow]}>Hello World Im yellow!</Text>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  blue: {
+    color: 'blue',
+    fontSize: 25,
+  },
+  red: {
+    color: 'red',
+    fontWeight: 'bold',
+  },
+  yellow: {
+   color: 'yellow',
+  fontSize: 10,
   },
 });

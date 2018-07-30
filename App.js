d11a9e3e7faa764f40dcc8e16c1976a698d521fd
GettyOrawo <gettyorawo@gmail.com>
2018-07-30 12:33:26 +0300

    Learn Props

--- App.js
+++ App.js
@@ -1,21 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component  } from 'react';
+import { AppRegistry, Text, View  } from 'react-native';
 
-export default class App extends React.Component {
+class Greeting extends Component {
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
-      </View>
+          <Text>Hello {this.props.name}!</Text>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class LotsOfGreetings extends Component {
+  render() {
+    return (
+          <View style={{alignItems: 'center'}}>
+              <Greeting name='Getty' />
+              <Greeting name='Orawo' />
+              <Greeting name='Gee' />
+            </View>
+    );
+  }
+}
+AppRegistry.registerComponent('AwesomeProject', () => LotsOfGreetings);

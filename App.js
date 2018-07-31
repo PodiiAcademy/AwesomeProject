518b061b03889ab1f08a078915d972bcdad3c5a8
GettyOrawo <gettyorawo@gmail.com>
2018-07-31 09:17:36 +0300

    Justify Content

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { AppRegistry, Text, View } from 'react-native';
 
-export default class FlexDirection extends Component{
+/*export default class FlexDirection extends Component{
   render(){
     return(
       <View style={{flex: 1, flexDirection: 'column'}}>
@@ -12,6 +12,18 @@ export default class FlexDirection extends Component{
     );
   }
 };
-AppRegistry.registerComponent('AwesomeProject', () => 'FlexDirection')
+AppRegistry.registerComponent('AwesomeProject', () => 'FlexDirection')*/
 
+export default class JustifyContent extends Component{
+  render(){
+    return(
+    <View style={{flex: 1, flexDirection: 'column', justifyContent: 'space-between'}}>
+      <View style={{width: 30,height: 30,backgroundColor: 'black'}}/>
+      <View style={{width: 30,height: 30,backgroundColor: 'yellow'}}/>
+      <View style={{width: 30,height: 30,backgroundColor: 'grey'}}/>
+    </View>
+    );
+  }
+};
+AppRegistry.registerComponent('AwesomeProject', () => JustifyContent)
 

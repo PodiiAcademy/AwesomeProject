c4ffb1de5fe9d064fbb410551adf8eb586ec79d9
GettyOrawo <gettyorawo@gmail.com>
2018-07-31 11:16:44 +0300

    Learn scrolling

--- App.js
+++ App.js
@@ -1,18 +1,51 @@
 import React, { Component  } from 'react';
-import { AppRegistry, ScrollView, Image, Text  } from 'react-native';
+import { AppRegistry, SectionList, StyleSheet, Text, View  } from 'react-native';
 
-export default class IScrolledDownAndWhatHappenedNextShockedMe extends Component {
+export default class SectionListBasics extends Component {
   render() {
     return (
-      <ScrollView>
-      <Text style={{fontSize:96}}>Scroll me plz</Text>
-      <Text style={{fontSize:96}}>If you like</Text>
-      <Text style={{fontSize:96}}>Scrolling down</Text>
-      <Text style={{fontSize:96}}>What's the best</Text>
-      <Text style={{fontSize:96}}>Framework around?</Text>
-      <Text style={{fontSize:80}}>React Native</Text>
-      </ScrollView>
+      <View style={styles.container}>
+      <SectionList
+      sections={[
+        {title: 'D', data: ['Devin']},
+        {title: 'J', data: ['Jackson', 'James', 'Jillian', 'Jimmy', 'Joel', 'John', 'Julie']},
+
+      ]}
+      renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
+      renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
+      keyExtractor={(item, index) => index}
+      />
+      </View>
+
     );
+
   }
+
 }
-AppRegistry.registerComponent('AwesomeProject', () => IScrolledDownAndWhatHappenedNextShockedMe);
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 22
+
+  },
+  sectionHeader: {
+    paddingTop: 2,
+    paddingLeft: 10,
+    paddingRight: 10,
+    paddingBottom: 2,
+    fontSize: 14,
+    fontWeight: 'bold',
+    backgroundColor: 'rgba(247,247,247,1.0)',
+
+  },
+  item: {
+    padding: 10,
+    fontSize: 18,
+    height: 44,
+
+  },
+
+})
+
+AppRegistry.registerComponent('AwesomeProject', () => SectionListBasics);

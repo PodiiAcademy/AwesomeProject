012375372cbae549492f3386c74ac6f157f77949
GettyOrawo <gettyorawo@gmail.com>
2018-07-31 10:27:45 +0300

    Learn Button Basics

--- App.js
+++ App.js
@@ -1,21 +1,43 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, Alert, Button, StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
-  render() {
+export default class ButtonBasics extends React.Component {
+    _onPressButton(){
+      Alert.alert('You Tapped the Button!')
+    }
+  render(){
     return (
-      <View style={styles.container}>
-        <Text>Hello World!</Text>
+    <View style={styles.container}>
+      <View style={styles.buttonContainer}>
+        <button onPress={this._onPressButton} title = "Press Me!" />
+      </View>
+      <View style={styles.buttonContainer}>
+        <Button onPress={this._onPressButton} title="Press Me" color="#841584" />
+      </View>
+      <View style={styles.alternativeLayoutButtonContainer}>
+        <Button onPress={this._onPressButton} title="This looks great!" />
+        <Button onPress={this._onPressButton} title="OK!" color="#841584" />
+      </View>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+container: {
+  flex: 1,
     justifyContent: 'center',
+
+},
+  buttonContainer: {
+    margin: 20
+
   },
-});
+  alternativeLayoutButtonContainer: {
+    margin: 20,
+      flexDirection: 'row',
+      justifyContent: 'space-between'
+
+  },
+})
+AppRegistry.registerComponent('AwesomeProject',() => ButtonBasics);
